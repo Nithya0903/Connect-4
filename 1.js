1ebadd9922e8a3d84239b919c6bf6896e1e26455
Nithya Manoj <nithyamanoj.ms@gmail.com>
2020-01-16 22:04:24 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,47 @@
+var player1 = prompt("Player One: Enter Your Name , you will be Blue");
+var player1Color = 'rgb(86, 151, 255)';
+
+var player2 = prompt("Player Two: Enter Your Name, you will be Red");
+var player2Color = 'rgb(237, 45, 73)';
+
+var game_on = true;
+var table = $('table tr');
+
+function reportWin(rowNum,colNum){
+  console.log("you won starting at this r,c");
+  console.log(rowNum);
+  console.log(colNum);
+}
+function changeColor(rowIndex,colIndex,color)
+{
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+}
+function returnColor(rowIndex,colIndex)
+{
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
+}
+function checkBottom(colIndex)
+{
+  var colorReport ;
+  for( var row = 6;row > -1;row--)
+  {
+    colorReport = returnColor(row,colIndex);
+    if(colorReport === 'rgb(128, 128, 128)'){
+      return row;
+    }
+  }
+}
+
+function colorMatchCheck(one,two,three,four)
+{
+  return(one === two && one === three && one === four && one !=='rgb(128, 128, 128)' && one !=undefined);
+}
+  function hcheck()
+  {   for(var row = 0; row<6;row++)
+    {  for(var col = 0;col<4;col++)
+      {
+
+
+      }
+    }
+  }
